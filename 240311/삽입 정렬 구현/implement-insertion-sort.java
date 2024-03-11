6cba654db3ab2ac11f43ccab9df8596f38410fd6
Kim Hak Hyeon 95 <81364044+KimHaKyeon95@users.noreply.github.com>
2024-03-11 21:55:11 +0900

    Title: 삽입 정렬 구현, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240311/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N; i++) {
+            int j = i - 1;
+            int key = arr[i];
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    
+    }
+}
